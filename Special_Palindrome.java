b1e3008966196ba9b49a872dcc2ff30d875838c3
Akarshan Gandotra <akarshan.gandotra@gmail.com>
2019-07-22 20:15:21 +0530

    Create Special_Palindrome.java

---
+++ Special_Palindrome.java
@@ -0,0 +1,31 @@
+class Solution {
+    
+    public boolean isPalindrome(long num) {
+        String text = String.valueOf(num);
+        if (text == null) {
+            return false;
+        }
+        int left = 0;
+        int right = text.length() - 1;
+        while (left < right) {
+            if (text.charAt(left++) != text.charAt(right--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public int superpalindromesInRange(String L, String R) {
+        int count = 0;
+        long l = (int) Math.sqrt(Long.parseLong(L));
+        long r = (int) Math.sqrt(Long.parseLong(R));
+        for(long n = l; n <= r; n++) {
+            if(isPalindrome(n)) {
+                if(isPalindrome(n*n))
+                    count++;
+            }
+        }             
+        return count;
+        
+    }
+}
